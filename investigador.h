0efe21e5af4340853eb17a9325519c6832031909
Dany Cheong <danycheong@unitec.edu>
2015-03-13 14:49:50 -0600

    Investigador

---
+++ investigador.h
@@ -0,0 +1,20 @@
+#include <string>
+#include "personas.h"
+
+using std::string;
+
+class Investigador:public Personas{
+	int casos;
+	int casos_cerrados;
+	int casos_sin_resolver;
+public:
+	Investigador(int,int,int);
+	Investigador(const Investigador&);
+	int getCasos();
+	int getCerrados();
+	int getSinResolver();
+	void setCasos(int);
+	void setCerrados(int);
+	void setSinResolver(int);
+	string toString();
+}
